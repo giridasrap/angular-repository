8aac93eff030f2f16e2c44dfb729e2d9a3ac703c
Giridhar Prasad <giridasrap@gmail.com>
2018-02-23 18:39:58 +0530

    Added the developer token to dialog-flow.ts file

--- src/app/dialog-flow-client/dialog-flow.client.ts
+++ src/app/dialog-flow-client/dialog-flow.client.ts
@@ -1,3 +1,3 @@
 import { ApiAiClient } from "api-ai-javascript";
 
-export const client = new ApiAiClient({accessToken: 'your-api-key-here'});
+export const client = new ApiAiClient({accessToken: '555a160ce88f474db030e393291cbb81'});
