a557b32d84cdd3edfff29df5ce5652de18deb0e6
Giridhar Prasad <giridasrap@gmail.com>
2018-02-23 19:18:06 +0530

    Corrected the code to pick the response from displayText instead of speech

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -23,7 +23,7 @@ export class ChatComponent {
       this.conversation.push({
         avatar: 'android',
         from: 'Bot',
-        content: response.result.fulfillment['speech'] || response.result.fulfillment['displayText']
+        content: response.result.fulfillment['displayText'] || 'I can\'t seem to figure that out!'
       });
       message.value = '';
     });
