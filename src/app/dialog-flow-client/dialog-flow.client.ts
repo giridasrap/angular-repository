2eb4e5a76febb0b11bcddde4cd60c0be6a18adec
giridasrap <36704259+giridasrap@users.noreply.github.com>
2018-02-26 01:08:13 +0530

    Update dialog-flow.client.ts
    Changed the token to point to the node-webhook application

--- src/app/dialog-flow-client/dialog-flow.client.ts
+++ src/app/dialog-flow-client/dialog-flow.client.ts
@@ -1,3 +1,3 @@
 import { ApiAiClient } from "api-ai-javascript";
 
-export const client = new ApiAiClient({accessToken: '555a160ce88f474db030e393291cbb81'});
+export const client = new ApiAiClient({accessToken: 'b5dc3259471044f88b4fbbc95b295723'});
